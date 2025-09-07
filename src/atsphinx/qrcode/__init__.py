801cb951f3d9d0f8747140958628512c29a4115b
Kazuya Takei <myself@attakei.net>
2025-09-08 03:54:46 +0900

    feat: Implement QRcode renderer

--- src/atsphinx/qrcode/__init__.py
+++ src/atsphinx/qrcode/__init__.py
@@ -2,7 +2,11 @@
 
 from __future__ import annotations
 
+import base64
 from typing import TYPE_CHECKING
+
+from docutils import nodes
+from sphinx.util.docutils import SphinxDirective
 
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
@@ -10,7 +14,26 @@ if TYPE_CHECKING:
 __version__ = "0.0.0"
 
 
+class QRodeDirective(SphinxDirective):  # noqa: D101
+    has_content = True
+
+    def run(self):  # noqa: D102
+        # NOTE: This is third-party library.
+        import qrcode
+        import qrcode.image.svg
+
+        content = "\n".join(self.content)
+        svg = qrcode.make(content, image_factory=qrcode.image.svg.SvgPathImage)
+        data = base64.b64encode(svg.to_string()).decode()
+        image = nodes.image(
+            alt=content.replace("\n", " "),
+            uri=f"data:image/svg+xml;base64,{data}",
+        )
+        return [image]
+
+
 def setup(app: Sphinx):  # noqa: D103
+    app.add_directive("qrcode", QRodeDirective)
     return {
         "version": __version__,
         "env_version": 1,
